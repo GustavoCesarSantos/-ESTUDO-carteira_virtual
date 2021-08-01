3bed59a25d5995e1036cdefe8109a8e22595c126
GustavoCS789 <gustavo.santos@berghem.com.br>
2021-08-01 00:10:27 -0300

    Criado repositorio da entidade financial transaction

---
+++ src/FinancialTransactionRepositoryMemory.ts
@@ -0,0 +1,30 @@
+import FinancialTransaction from './FinancialTransaction';
+import FinancialTransactionRepository from './FinancialTransactionRepository';
+
+const financialTransactionMock = new FinancialTransaction({
+  date: new Date('1995-12-17T03:24:00'),
+  cpf: '490.310.060-09',
+  category: 'Pagamento de conta',
+  observation: 'Conta de Luz de Outubro',
+  value: 100,
+});
+
+export default class FinancialTransactionRepositoryMemory implements FinancialTransactionRepository {
+  financialTransactions: FinancialTransaction[];
+
+  constructor() {
+    this.financialTransactions = [
+      financialTransactionMock,
+    ];
+  }
+
+  save(financialTransaction: FinancialTransaction): void {
+    this.financialTransactions.push(financialTransaction);
+  }
+
+  updateCategory({
+    date, walletIdentification, category, newCategory,
+  } : { date: Date, walletIdentification: string, category: string, newCategory: string }): void {
+    this.financialTransactions[0].category = newCategory;
+  }
+}
