bfabd8b5cf6dd4f127a9915c2c192d7e03bc6d15
GustavoCS789 <gustavo.santos@berghem.com.br>
2021-08-01 00:09:09 -0300

    Criado value transaction

---
+++ src/Transaction.ts
@@ -0,0 +1,19 @@
+export default class Transaction {
+  date: Date;
+
+  cpf: string;
+
+  category: string;
+
+  observation: string;
+
+  value: number;
+
+  constructor(date: Date, cpf: string, category: string, observation: string, value: number) {
+    this.date = date;
+    this.cpf = cpf;
+    this.category = category;
+    this.observation = observation;
+    this.value = value;
+  }
+}
