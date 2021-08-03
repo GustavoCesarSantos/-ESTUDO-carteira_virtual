ba7d7359a4cb2d385e6a5ba76be21c3441e9675f
GustavoCS789 <gustavo.santos@berghem.com.br>
2021-08-03 17:50:07 -0300

    Criado migration do usuario

---
+++ src/infra/database/migration/1628007376376-RegisterUsers.ts
@@ -0,0 +1,33 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class RegisterUsers1628007376376 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('users');
+  }
+}
