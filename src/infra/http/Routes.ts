d025f5db85eb48668c29cf18f6234001b6297ac8
GustavoCS789 <gustavo.santos@berghem.com.br>
2021-08-03 17:54:04 -0300

    Criado rotas para login e registro de usuario

--- src/infra/http/Routes.ts
+++ src/infra/http/Routes.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 import FinancialTransactionControllers from '../../FinancialTransaction/adapters/controllers/FinancialTransactionControllers';
+import UserControllers from '../../User/adapters/controllers/UserControllers';
 import VirtualWalletControllers from '../../VirtualWallet/adapters/controllers/VirtualWalletControllers';
 
 const router = Router();
 const virtualWalletControllers = new VirtualWalletControllers();
 const financialTransactionControllers = new FinancialTransactionControllers();
+const userControllers = new UserControllers();
 
-router.post('/wallets', virtualWalletControllers.RegisterVirtualWallerController);
-router.get('/wallets/:cpf/balance', virtualWalletControllers.ReturnWalletBalanceController);
+router.post('/users', userControllers.RegisterUserController);
+router.post('/login', userControllers.LoginController);
 
-router.post('/wallets/:cpf/withdrawal', financialTransactionControllers.performWithdrawal);
-router.post('/wallets/:cpf/deposit', financialTransactionControllers.performDeposit);
-router.put('/transactions/:transaction', financialTransactionControllers.changeCategory);
+router.post('/wallets', userControllers.isAuthorized, virtualWalletControllers.RegisterVirtualWallerController);
+router.get('/wallets/:cpf/balance', userControllers.isAuthorized, virtualWalletControllers.ReturnWalletBalanceController);
+
+router.post('/wallets/:cpf/withdrawal', userControllers.isAuthorized, financialTransactionControllers.performWithdrawal);
+router.post('/wallets/:cpf/deposit', userControllers.isAuthorized, financialTransactionControllers.performDeposit);
+router.put('/transactions/:transaction', userControllers.isAuthorized, financialTransactionControllers.changeCategory);
 
 export { router };
